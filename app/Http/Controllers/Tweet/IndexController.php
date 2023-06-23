6371372ddb5fb0ef36047bc9dfce46fe06b58bde
eszett-stdmt <s.sato@study-mate.co.jp>
2023-06-23 12:48:19 +0900

    END:: chapter 3-01

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -3,21 +3,22 @@
 namespace App\Http\Controllers\Tweet;
 
 use App\Http\Controllers\Controller;
+use App\Services\TweetService;
 use Illuminate\Http\Request;
 
-use App\Models\Tweet;
 
 class IndexController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, TweetService $tweetService)
     {
         //
-        $tweets = Tweet::orderBy('created_at', 'DESC')->get();
+        //$tweets = Tweet::orderBy('created_at', 'DESC')->get();
         // dd($tweets);
-        return view('tweet.index')
-            ->with('tweets', $tweets);
+        //$tweetService = new TweetService();
+        $tweets = $tweetService->getTweets();
+        return view('tweet.index')->with('tweets', $tweets);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -33,17 +33,11 @@ require __DIR__.'/auth.php';
 route::get('/sample', [\App\Http\Controllers\Sample\IndexController::class, 'show']);
 route::get('/sample/{id}', [\App\Http\Controllers\Sample\IndexController::class, 'showId']);
 
-route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)
-->name('tweet.index');
+route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)->name('tweet.index');
 
-route::post('/tweet/create', \App\Http\Controllers\Tweet\CreateController::class)
-->name('tweet.create');
+route::post('/tweet/create', \App\Http\Controllers\Tweet\CreateController::class)->name('tweet.create');
 
-route::get('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\IndexController::class)
-->name('tweet.update.index')
-->where('tweetId', '[0-9]+');
-route::put('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\PutController::class)
-->name('tweet.update.put')
-->where('tweetId', '[0-9]+');
+route::get('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\IndexController::class)->name('tweet.update.index')->where('tweetId', '[0-9]+');
+route::put('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\PutController::class)->name('tweet.update.put')->where('tweetId', '[0-9]+');
 
 Route::delete('/tweet/delete/{tweetId}', \App\Http\Controllers\Tweet\DeleteController::class)->name('tweet.delete');
